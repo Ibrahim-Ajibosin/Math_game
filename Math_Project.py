2a7874a2b6123e3a1e13e16b81acf7e67fa56254
Ibrahim Ajibosin <82901442+Ibrahim-Ajibosin@users.noreply.github.com>
2022-04-29 20:20:51 +0200

    Update Math_Project.py

--- Math_Project.py
+++ Math_Project.py
@@ -4,27 +4,29 @@
 
 from myfunctions import generate_question, get_user_point, updateUserPoints
 
+try :
+    name = input("Enter your username")
 
-name = input("Enter your username")
+    if not name.isalpha():
+        print("Your last name cant be a digit")
+    else:
+        user_point = int(get_user_point(name))
 
-if not name.isalpha():
-    print("Your last name cant be a digit")
-else:
-    user_point = int(get_user_point(name))
+        if user_point == -1 :
+            newUser = True
+        else :
+            newUser = False
 
-    if user_point == -1 :
-        newUser = True
-    else :
-        newUser = False
+        if newUser == True :
+            user_point = 0
+            
+        userChoice = 0
+        while userChoice != "-1":
+            user_point += generate_question()
+            user_input = input("Enter -1 to end the game or 0 to continue ")
+            userChoice = user_input
 
-    if newUser == True :
-        user_point = 0
-        
-    userChoice = 0
-    while userChoice != "-1":
-        user_point += generate_question()
-        user_input = input("Enter -1 to end the game or 0 to continue ")
-        userChoice = user_input
-
-    ####### Update the userScores.txt file ##########
-    updateUserPoints(newUser,name,str(user_point))
+        ####### Update the userScores.txt file ##########
+        updateUserPoints(newUser,name,str(user_point))
+except: 
+      print ("Please rerun the program, something went wrong")
